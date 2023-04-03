fc69d1279559446314373ad4fc8008e902f63490
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 12:28:30 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -2,29 +2,21 @@ import numpy as np
 import streamlit as st
 import time
 import pandas as pd
+import plotly.express as px
+
+
 
 #testing 1
 st.header = ('holas')
 st.write('Hello world!')
 
 
-# Get some data.
-data = np.random.randn(10, 2)
+uploaded_file = st.file_uploader('Elija archivo xlsx', type='xlsx')
 
-# Show the data as a chart.
-chart = st.line_chart(data)
-
-# Wait 1 second, so the change is clearer.
-time.sleep(1)
-
-# Grab some more data.
-data2 = np.random.randn(10, 2)
-
-# Append the new data to the existing chart.
-chart.add_rows(data2)
-
-#testing code web
-st.title('DESESTIMIENTOS ATET')
+if uploaded_file :
+  st.markdown('---')
+  df = pd.read_excel(uploaded_file, engine='openpyxl')
+  st.dataframe(df)
 
 
 
