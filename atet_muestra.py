a2731afaff3a6a1aecf52fb23b79d91800ce9540
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 11:39:30 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -25,10 +25,5 @@ data2 = np.random.randn(10, 2)
 # Append the new data to the existing chart.
 chart.add_rows(data2)
 
-#upload file 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+
 
