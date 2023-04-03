74faacb70f580162232099e3eacb230e43194be5
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 17:23:52 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -2,7 +2,7 @@ import numpy as np
 import streamlit as st
 import time
 import pandas as pd
-from io import StringIO
+from st_aggrid import AgGrid
 
 #testing 1
 st.header = ('holas')
@@ -11,20 +11,13 @@ st.write('Hello world!')
 
 
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+@st.cache_data()
+def load_data():
+    data = pd.read_csv('./ListDesestimiento1.csv', parse_dates=['referenceDate'])
+    return data
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+data = load_data()
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+AgGrid(data, height=400)
+
