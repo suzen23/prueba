1689cfcba1c5c1b6b58fa75d4d64602bbf7e2cf7
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 00:24:56 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -1,13 +1,9 @@
 
 import pandas as pd
 import numpy as np
-
 import streamlit as st
 
-excel_file = 'ListDesestimiento.xlsx'
-sheet_name='DATA'
 
-df = pd.read_excel(excel_file,
-                   sheet_name = sheet_name,
-                   usecols='B:D',
-                   header =3)
+st.header ='holas'
+
+
