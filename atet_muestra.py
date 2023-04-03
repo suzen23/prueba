f5933f9b8d42e7d4926af62882cf0d444406f74f
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 17:26:39 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -4,20 +4,11 @@ import time
 import pandas as pd
 from st_aggrid import AgGrid
 
+
+
+
 #testing 1
 st.header = ('holas')
 st.write('Hello world!')
 
-
-
-
-
-@st.cache_data()
-def load_data():
-    data = pd.read_csv('./ListDesestimiento1.csv', parse_dates=['referenceDate'])
-    return data
-
-data = load_data()
-
-AgGrid(data, height=400)
 
