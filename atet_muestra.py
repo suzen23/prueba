77f723f643a4dd6ee01d57f595cc7d98500cfe20
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 11:21:57 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -1,8 +1,8 @@
-
-import pandas as pd
 import numpy as np
 import streamlit as st
 import time
+import pandas as pd
+from io import StringIO
 
 st.header = ('holas')
 st.write('Hello world!')
@@ -24,3 +24,25 @@ data2 = np.random.randn(10, 2)
 
 # Append the new data to the existing chart.
 chart.add_rows(data2)
+
+
+
+#testing code
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
