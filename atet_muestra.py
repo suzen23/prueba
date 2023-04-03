5618567935928afe073ec0cd220a17bb711b3b74
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-02 23:55:20 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -3,4 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-excel_file = ''
+excel_file = 'ListDesestimiento.xlsx'
+sheet_name='DATA'
+
+df = pd.read_excel(excel_file,
+                   sheet_name = sheet_name,
+                   usecols='B:D',
+                   header =3)
