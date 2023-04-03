9d030668263f7805bd55c7ae01b18ecab00c25f1
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 11:11:05 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -2,8 +2,25 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+import time
 
 st.header = ('holas')
 st.write('Hello world!')
 
+
+
+
+# Get some data.
+data = np.random.randn(10, 2)
+
+# Show the data as a chart.
+chart = st.line_chart(data)
+
+# Wait 1 second, so the change is clearer.
+time.sleep(1)
+
+# Grab some more data.
+data2 = np.random.randn(10, 2)
+
+# Append the new data to the existing chart.
+chart.add_rows(data2)
