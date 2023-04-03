7a7f320116867234b385d8b38b942f1dd41a58d3
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-03 12:37:50 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -11,12 +11,19 @@ st.header = ('holas')
 st.write('Hello world!')
 
 
-uploaded_file = st.file_uploader('Elija archivo xlsx', type='xlsx')
+st.subheader("Define a custom colorscale")
+df = px.data.iris()
+fig = px.scatter(
+    df,
+    x="sepal_width",
+    y="sepal_length",
+    color="sepal_length",
+    color_continuous_scale="reds",
+)
 
-if uploaded_file:
-  st.markdown('---')
-  df = pd.read_excel(uploaded_file, engine='openpyxl')
-  st.dataframe(df)
-
-
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    st.plotly_chart(fig, theme=None, use_container_width=True)
 
