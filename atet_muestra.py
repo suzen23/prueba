7eb2ab8ef14319df84cbbfb83e918f1e9eb6c0f0
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-05 16:36:30 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -3,7 +3,7 @@ import streamlit as st
 import time
 import pandas as pd
 streamlit run atet_muestra.py
-
+import time
 
 
 #testing 1
@@ -16,3 +16,21 @@ num2= 20
 sum = num1+num2
 
 print (sum)
+
+
+
+
+# Get some data.
+data = np.random.randn(10, 2)
+
+# Show the data as a chart.
+chart = st.line_chart(data)
+
+# Wait 1 second, so the change is clearer.
+time.sleep(1)
+
+# Grab some more data.
+data2 = np.random.randn(10, 2)
+
+# Append the new data to the existing chart.
+chart.add_rows(data2)
