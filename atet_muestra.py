df952ad4358bc976a17300e162974c30d22e558c
suzen23 <129707890+suzen23@users.noreply.github.com>
2023-04-05 17:09:46 -0500

    Update atet_muestra.py

--- atet_muestra.py
+++ atet_muestra.py
@@ -22,7 +22,8 @@ num1 =st.number_input('Introduzac un numero:')
 
 st.write('El numero al cuadrado es ', num1 ** 2)
 
-
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
 
 
 
